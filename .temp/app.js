0b26906a069c7b26063ffb6cbcd731a8b1491e3d
Xin <xh23@rice.edu>
2020-06-07 16:49:33 -0500

    just for merge conflict

--- .temp/app.js
+++ .temp/app.js
@@ -5,10 +5,6 @@ import './app.scss';
 
 // 如果需要在 h5 环境中开启 React Devtools
 // 取消以下注释：
-// if (process.env.NODE_ENV !== 'production' && process.env.TARO_ENV === 'h5')  {
-//   require('nerv-devtools')
-// }
-
 import Nerv from 'nervjs';
 import { View, Tabbar, TabbarContainer, TabbarPanel } from '@tarojs/components';
 import { Router, createHistory, mountApis } from '@tarojs/router';
